65b57126707f93ad66d8b958f46bbc4cf0d90868
takeru-segawa <takeru.t@itcgroup.io>
2025-01-20 14:49:18 +0700

    Add rating service

---
+++ src/main/java/com/example/microservice/ratingservice/models/Rating.java
@@ -0,0 +1,54 @@
+package com.example.microservice.ratingservice.models;
+
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="ratings")
+public class Rating {
+    @Id
+    public String id;
+    public Integer userId;
+    public Integer movieId;
+    public Integer rating;
+    public Long timestamp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
